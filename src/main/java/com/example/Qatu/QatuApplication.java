8700e83972abd50b53fdac384a19e7f79f3184da
Angelo <angelo.cabrera01@hotmail.com>
2026-04-25 15:45:18 -0500

    iniciamos el back

---
+++ src/main/java/com/example/Qatu/QatuApplication.java
@@ -0,0 +1,13 @@
+package com.example.Qatu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QatuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QatuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Qatu/QatuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Qatu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QatuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
